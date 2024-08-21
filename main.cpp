890db5d4e61d48cf85a15c214fce5d5b1334dd93
Alexander Aksenov <evenreven@mail.ru>
2024-08-21 13:02:06 +0400

    SSH test

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QLabel *label = new QLabel("Hello world!");
+    QLabel *label = new QLabel("Hello world ssh!");
     label->show();
 
     return a.exec();
